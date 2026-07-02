02de3c8395c9821ed2e6edb1306605243ce49332
Haffiz, Muzzammil Saleh Mohamed <muzzammilsaleh.haffiz@strathmore.edu>
2026-07-02 14:20:07 +0300

    Add click to reveal banner feature

--- submission/js/script.js
+++ submission/js/script.js
@@ -16,3 +16,10 @@ testimonials.forEach(function(testimonial) {
     li.appendChild(quoteParagraph);
     testimonialsList.appendChild(li);
 });
+
+let bannerImage = document.querySelector("#bannerImage");
+let bannerCaption = document.querySelector("#bannerCaption");
+
+bannerImage.addEventListener("click", function () {
+    bannerCaption.classList.toggle("show");
+});
